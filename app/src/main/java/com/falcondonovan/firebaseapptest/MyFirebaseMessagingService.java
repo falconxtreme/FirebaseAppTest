0048ad3dc2f96d8893d57eb6d81ea368d722ee64
Eder Donovan Ramos Morales <falcondonovan@Eders-MacBook-Pro.local>
2017-05-04 00:43:34 -0500

    Initial commit

---
+++ app/src/main/java/com/falcondonovan/firebaseapptest/MyFirebaseMessagingService.java
@@ -0,0 +1,18 @@
+package com.falcondonovan.firebaseapptest;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by falcondonovan on 5/2/17.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.e("FIREBASE", remoteMessage.getNotification().getBody());
+    }
+}
