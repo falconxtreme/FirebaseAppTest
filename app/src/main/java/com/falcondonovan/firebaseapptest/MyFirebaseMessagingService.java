8ae93087d9dcda098e9b6fff911f5e2fb8edbe60
falconxtreme <falcon.donovan@gmail.com>
2017-05-04 00:50:49 -0500

    Edicion de archivo de message

--- app/src/main/java/com/falcondonovan/firebaseapptest/MyFirebaseMessagingService.java
+++ app/src/main/java/com/falcondonovan/firebaseapptest/MyFirebaseMessagingService.java
@@ -6,10 +6,11 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 /**
- * Created by falcondonovan on 5/2/17.
+ * Created by falcondonovan on 5/2/17. changed now
  */
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
